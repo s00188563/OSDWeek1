1d13d54f6ffeb01ed5d7686ff3023bbdb37aaceb
s00188563 <fernandezluigi10@yahoo.ie>
2022-01-27 16:41:16 +0000

    Task 2 done

--- src/app/basics/compute.spec.ts
+++ src/app/basics/compute.spec.ts
@@ -5,4 +5,8 @@ describe('compute', () => {
     const result = compute(-1);
     expect(result).toBe(0);
   });
+  it('should return 1 if the input is positive', () => {
+    const result = compute(1);
+    expect(result).toBe(2);
+  });
 });
